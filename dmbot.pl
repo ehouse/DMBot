ac250434dbf558e1a7a7847795af9d42696743cf
Ethan House <ehouse@csh.rit.edu>
2013-12-03 23:49:43 -0800

    Init Commit
    DMBot works!

---
+++ dmbot.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+package DMBot;
+use base qw(Bot::BasicBot);
+
+my $dm = DMBot->new(
+    server      =>  "skynet.csh.rit.edu",
+    port        =>  "6667",
+    channels    =>  ["#DND"],
+    
+    nick        =>  "DMBot",
+    alt_nicks   =>  ["The_Real_DMBot"],
+    username    =>  "DMBot",
+    name        =>  "D&D DM Bot",
+
+    ignore_list =>  [qw(RepBot ChanServ)],
+);
+
+sub said {
+    my ($self, $message) = @_;
+    if ($message->{body} =~ /^!rtd(\d+)?/){
+        if (defined $1){
+            return int(rand($1))+1
+        }
+        else{
+            return int(rand(20))+1
+        }
+    }
+}
+
+$dm->run();
